8993adc231feaa68be185cba0ec45379ee30982b
simsangho6448 <mjj00travel@gmail.com>
2025-02-12 17:29:53 +0900

    feat: health 추가

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from app.consumer import start_consumer
 import asyncio
 
+@app.get("/health")
+async def health_check():
+    return {"status": "ok"}
+
 if __name__ == '__main__':
     asyncio.run(start_consumer())
