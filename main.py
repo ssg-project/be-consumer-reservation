8b32d84934295d2c859dd88ecf3597840a9f254b
simsangho <mjj00travel@gmail.com>
2025-02-13 15:13:02 +0900

    health check 삭제

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
 from app.consumer import start_consumer
-from fastapi import FastAPI
 import asyncio
-
-app = FastAPI() 
-
-@app.get("/health")
-async def health_check():
-    return {"status": "ok"}
 
 if __name__ == '__main__':
     asyncio.run(start_consumer())
