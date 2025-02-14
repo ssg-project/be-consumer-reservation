7bba54ea3516170d1b18f1f16c187f2643e54c2e
sh167603 <soohwan06@gmail.com>
2025-02-14 15:46:07 +0900

    다시 수정

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
-from fastapi import FastAPI
 from app.consumer import start_consumer
-from ticket_router import ticket_router
 import asyncio
-import uvicorn
-
-app = FastAPI()
-
-# health check route
-@app.get("/health")
-async def health_check():
-    return {"status": "ok"}
-
-# router 설정
-app.include_router(ticket_router, prefix="/api/v1", tags=["ticket"])
 
 if __name__ == '__main__':
     uvicorn.run(app, host="0.0.0.0", port=9093)
