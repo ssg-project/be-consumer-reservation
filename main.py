9140c4ad465a45439ab473f2e8cff39d1abcfad5
sh167603 <soohwan06@gmail.com>
2025-02-20 15:24:05 +0900

    port 변경

--- main.py
+++ main.py
@@ -4,8 +4,8 @@ from prometheus_client import start_http_server, Gauge
 from app.consumer import start_consumer
 
 if __name__ == '__main__':
-    # Prometheus HTTP 서버 시작 (8000 포트)
-    start_http_server(8000)
+    # Prometheus HTTP 서버 시작 (9093 포트)
+    start_http_server(9093)
     
     # Kafka Consumer 시작
     asyncio.run(start_consumer())
