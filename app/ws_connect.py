22939fe9d3f7efca6781e42fdc0b9e9aa9ddfaa2
kuni4210 <kuni4210@naver.com>
2025-02-24 12:32:55 +0900

    log 추가

--- app/ws_connect.py
+++ app/ws_connect.py
@@ -1,6 +1,13 @@
 from app.config import WEBSOCKET_SERVER_URL
 import websockets
 import json
+import logging
+
+logging.basicConfig(
+    filename='debug.log',
+    level=logging.DEBUG,
+    format='%(asctime)s - %(message)s'
+)
 
 async def ws_send_success(user_id, concert_id):
     data = {
@@ -26,10 +33,14 @@ async def send_to_websocket_server(data):
     try:
         async with websockets.connect(WEBSOCKET_SERVER_URL) as ws:
             await ws.send(json.dumps(data))
+
             print(f"Message sent to WebSocket server: {data}")
+            logging.debug(f"Message sent to WebSocket server: {data}")
 
             response = await ws.recv()
             print(response)
     except Exception as e:
         print(WEBSOCKET_SERVER_URL)
         print(f"Failed to send message to WebSocket server: {e}")
+        logging.debug(WEBSOCKET_SERVER_URL)
+        logging.debug(f"Failed to send message to WebSocket server: {e}")
